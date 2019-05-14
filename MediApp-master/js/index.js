8c82ede3e9e1cea758c77c55374d780fd2186995
aarrc980 <47569648+aarrc980@users.noreply.github.com>
2019-05-14 17:50:20 -0500

    Add files via upload

---
+++ MediApp-master/js/index.js
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */var pantalladecarga;
+ var pantallaprincipal;
+
+ window.onload = inicio;
+function inicio(){
+
+    inicializarvariables();
+    main();
+}
+
+function main(){
+    
+    mostrarpantalla(pantalladecarga);
+    setTimeout("cambiopantalla(pantallaprincipal,pantalladecarga)", 1500);
+    }
+
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+
+        document.getElementById("btnMedicamentos").addEventListener("click", mostrarMedicamentos);
+        document.getElementById("btncitas").addEventListener("click", mostrarcitas);
+
+        //acá escucha los botones--------------------------------------------------------------------
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+
+
+
+
+function inicializarvariables(){
+ pantalladecarga=document.getElementById("pantallacarga");
+ pantallaprincipal=document.getElementById("inicio");
+
+}
+
+
+
+    function mostrarpantalla(pantalla){
+    pantalla.className=pantalla.className.replace( /(?:^|\s)ocultar(?!\S)/g , '' );
+  }
+
+ function cambiopantalla(pantalla, pantallaanterior){
+    pantallaanterior.className +=" ocultar";
+    pantalla.className=pantalla.className.replace( /(?:^|\s)ocultar(?!\S)/g , '' );
+  }
+function ocultar(){
+
+    document.getElementById("pantallacarga").className = "ocultar";
+    document.getElementById("inicio").className = "ocultar";
+    document.getElementById("divMedicamentos").className = "ocultar";
+}
+
+function mostrarInicio(){
+
+    ocultar();
+    document.getElementById("inicio").className = "Pantalla1 animated fadeIn";
+}
+
+function mostrarMedicamentos(){
+
+    ocultar();
+    document.getElementById("divMedicamentos").className = "Pantalla2 animated fadeIn";
+}
+function mostrarcitas(){
+
+    ocultar();
+    document.getElementById("citas").className = "Pantalla2 animated fadeIn";
+}
+//acá pone las funciones-------------------------------------------------------------------------------------
+
+app.initialize();
